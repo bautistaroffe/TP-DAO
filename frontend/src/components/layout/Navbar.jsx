f0cdf6a3c8d062da96df4024265effb539d4d7ef
manupereiraduarte <98099@sistemas.frc.utn.edu.ar>
2025-11-11 15:22:06 -0300

    division de vistas y agrego aclaracion de eso en readme

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,11 +1,17 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function Navbar({ isOpen }) {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+
   return (
     <nav className={`navbar ${isOpen ? "show" : ""}`}>
-      <NavLink to="/" end>Inicio</NavLink>
-      <NavLink to="/predio">Predio</NavLink>
-      <NavLink to="/reportes">Reportes</NavLink>
+      {!isHome && (
+        <>
+          <NavLink to="/predio">Predio</NavLink>
+          <NavLink to="/reportes">Reportes</NavLink>
+        </>
+      )}
     </nav>
   );
 }
